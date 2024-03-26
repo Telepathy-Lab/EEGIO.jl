2385a871d1e084e3531b2bf78a8b10769a1970d0
Marcin Koculak <koculak.marcin@gmail.com>
2024-03-26 23:15:55 +0100

    Add docstrings to functions reading SET files

--- src/formats/SET.jl
+++ src/formats/SET.jl
@@ -1,10 +1,33 @@
+"""
+    SETChannels(
+        labels::Vector{String},
+        type::Vector{String},
+        ref::Vector{String},
+        identifier::Vector{Int64},
+        description::Vector{String},
+        urchan::Vector{Float64},
+        X::Vector{Float64},
+        Y::Vector{Float64},
+        Z::Vector{Float64},
+        theta::Vector{Float64},
+        radius::Vector{Float64},
+        sph_phi::Vector{Float64},
+        sph_radius::Vector{Float64},
+        sph_theta::Vector{Float64}
+    )
+
+Struct for storing channel information from SET files.
+Follows roughly the specification that can be found in eeg_checkchanlocs.m file from EEGLab.
+Only channel labels seem to be obligatory, the rest is optional and can be left empty.
+Does not include legacy/depracated fields as EEGLab mostly omits them itself.
+"""
 mutable struct SETChannels
     labels::Vector{String}
     type::Vector{String}
     ref::Vector{String}
     identifier::Vector{Int64}
     description::Vector{String}
-    urchan::Vector{Any}
+    urchan::Vector{Float64}
     X::Vector{Float64}
     Y::Vector{Float64}
     Z::Vector{Float64}
@@ -15,11 +38,63 @@ mutable struct SETChannels
     sph_theta::Vector{Float64}
 end
 
+"""
+    SETChannels()
+
+Empty SETChannels constructor. All fields are initialized as empty vectors of the appropriate type.
+"""
 SETChannels() = SETChannels(
     String[],String[],String[],Int[],String[], Any[],
     Float64[],Float64[],Float64[],Float64[],Float64[],Float64[],Float64[],Float64[]
 )
 
+"""
+    SETHeader(
+        setname::String,
+        trials::Int64,
+        pnts::Int64,
+        nbchan::Int64,
+        srate::Float64,
+        xmin::Float64,
+        xmax::Float64,
+        times::Vector{Float64},
+        ref::Union{String, Int64},
+        history::Union{String, Vector{String}},
+        comments::String,
+        etc::Dict{String, Any},
+        saved::Union{String, Bool},
+        raw::Dict{String, Any},
+        datfile::String,
+        chanlocs::SETChannels,
+        urchanlocs::Dict{String, Any},
+        chaninfo::Dict{String, Any},
+        splinefile::String,
+        event::Dict{String, Any},
+        urevent::Dict{String, Any},
+        epoch::Dict{String, Any},
+        eventdescription::Vector{String},
+        epochdescription::Vector{String},
+        icasphere::Array,
+        icaweights::Array,
+        icawinv::Array,
+        icaact::Array,
+        icasplinefile::String,
+        icachansind::Vector{Int64},
+        dipfit::Dict{String, Any},
+        subject::String,
+        group::String,
+        condition::String,
+        run::Int,
+        session::Int,
+        specdata::Array,
+        specicaact::Array,
+        stats::Dict{String, Any},
+        reject::Dict{String, Any}
+    )
+
+Struct for storing EEG header information.
+Follows roughly the specification that can be found in eeg_checkset.m file from EEGLab.
+"""
 mutable struct SETHeader <: Header
     # Basic information
     setname::String
@@ -35,6 +110,7 @@ mutable struct SETHeader <: Header
     comments::String
     etc::Dict{String, Any}
     saved::Union{String, Bool}
+    raw::Dict{String, Any}
     # Data file
     datfile::String
     # Chennel locations
@@ -69,14 +145,27 @@ mutable struct SETHeader <: Header
     reject::Dict{String, Any}
 end
 
+"""
+    SETHeader()
+
+Empty SETHeader constructor. All fields are initialized as empty values of the appropriate type.
+"""
 # This is extremally ugly, but can be updated afterwards, reducing the need for a huge constructor later
 SETHeader() = SETHeader(
-    "", 0, 0, 0, 0., 0., 0., Float64[], 0, String[], "", Dict(), false,
+    "", 0, 0, 0, 0., 0., 0., Float64[], 0, String[], "", Dict(), false, Dict(),
     "", SETChannels(), Dict(), Dict(), "", Dict(), Dict(), Dict(), String[], String[],
     zeros(0,0), zeros(0,0), zeros(0,0), zeros(0,0), "", Int64[], Dict(),
     "", "", "", 0, 0, zeros(0,0), zeros(0,0), Dict(), Dict()
 )
 
+"""
+    SET(header::SETHeader, data::Array, path::String, file::String)
+
+Struct for storing EEG data from SET files.
+Header contains all metadata from the .set file. Data is a 2D array (samples x channels).
+Currently, no convenient constructors are provided, as the writing to file functionality 
+is not yet implemented.
+"""
 mutable struct SET <: EEGData
     header::SETHeader
     data::Array
--- src/load/load_set.jl
+++ src/load/load_set.jl
@@ -1,3 +1,64 @@
+"""
+    read_set(f::String; kwargs...)
+
+Reads EEG data from a SET file.
+
+Providing a string containing valid path to a .set or .fdt file will result in reading 
+the data as a Float64 matrix (provided all files share their name). Behavior of the function 
+can be altered through additional keyword arguments listed below. Please consult the online 
+documentation for a more thorough explanation of different options.
+
+## Arguments:
+- `f::String`
+    - Path to the SET file (either .set or .fdt) to be read.
+
+## Keywords:
+- `onlyHeader::Bool=false`
+    - Indicates whether to read only the metadata information from the .set file.
+- `numPrecision::Type=Float64`
+    - Specifies the numerical type of the data. Data points in SET and FDT files are stored 
+      as 32-bit floats, therefore can be read as types with higher bit count per number.
+      However, currently data stored within .set files is always read as Float64 (limitations
+      of MAT.jl library that this package depends on) therefore ignoring this setting. For
+      data stored in separate .fdt files, possible tested options are: `Float32`, `Float64`, 
+      `Int32`, `Int64`. Since there is no clear benefit (except amount of memory used) to 
+      using smaller number, the default is set to `Float64`.
+- `chanSelect::Union{Int, Range, Vector{Int}, String, Vector{String}, Regex, Symbol}=:All`
+    - Specifies the subset of channels to read from the data. Depending on the provided value
+      you can select different subsets of channels.
+      Using integers (either single number, range, or a vector) will select channels by their
+      position in the data (e.g. chanSelect=[1,4,8], will pick the first, fourth, and eighth).
+      Using strings or regex expression will pick all the channels with matching names stored
+      in the header.
+      Finally, you can provide symbols :None or :All to read neither or every channel available.
+      Default is set to :All.
+- `chanIgnore::Union{Int, UnitRange, Vector{Int}, String, Vector{String}, Regex, Symbol}=:None`
+    - Specifies the subset of channel to omit while reading the data.
+      Uses the same selectors as `chanSelect` and picked values are subtracted from the set
+      of electrodes chosen by `chanSelect`. Default is set to :None.
+- `timeSelect::Union{Int, UnitRange, Tuple{AbstractFloat}, Symbol}=:All`
+    - Specifies the part of the time course of the data to be read.
+      Using integers will select samples with those indexes in the data.
+      Using a tuple of floats will be interpreted as start and stop values in seconds
+      and all samples that fit this time span will be read.
+      Using Symbol :All will read every sample in the file. This is also the default.
+- `method::Symbol=:Direct`
+    - Choose the IO method of reading data. Can be either :Direct (set as the default) for 
+      reading the file in chunks or :Mmap for using memory mapping. This setting is ignored
+      for .set files, as they are always read in directly by MAT.jl.
+- `tasks::Int=1`
+    - Specifies how many concurent tasks will be spawn to read chunks of data. Tasks will use
+      as many threads as there are available to Julia, but users can specify a higher number.
+      Default is set to 1 (equal to single threaded run).
+
+SET files are just MATLAB data structures saved in a file with a .set extension. They have
+a specific structure imposed by the EEGLab software, but individual users might alter it
+manually to contain additional fields or data types. There were also changes in the format
+over the years, so there is always a possibility that some data were not extracted.
+MAT.jl library reads in the data as a single dictionary, which is then parsed by this function.
+To allow users to inspect all of the original metadata, it is being stored in the header field
+`raw`. Please check it if you suspect some data are missing.
+"""
 function read_set(f::String; kwargs...)
     # Check if an existing path is given, then open the file
     if isfile(f)
@@ -32,6 +93,7 @@ function read_set(fid; path="", file="", onlyHeader=false, addOffset=true, numPr
     # Read the header
     header, rawData = read_set_header(raw)
 
+    # If the data is stored in a separate file, read it
     if typeof(rawData) == String
         if onlyHeader || isempty(rawData)
             data = Array{Float64}(undef, (0,0))
@@ -52,8 +114,11 @@ function read_set(fid; path="", file="", onlyHeader=false, addOffset=true, numPr
             data = read_set_data(rawDataPath, header, numPrecision, chanSelect, chanIgnore, timeSelect, method, tasks)
         end
 
+    # If the data is already in the file, read it
     elseif typeof(rawData) <: AbstractArray
         data = read_set_data(rawData, header, numPrecision, chanSelect, chanIgnore, timeSelect, tasks)
+        # Remove the raw signal data from the header
+        header.raw["data"] = ""
     else
         error("EEG data in file is of unknown type: $(type(rawData)).")
     end
@@ -61,6 +126,7 @@ function read_set(fid; path="", file="", onlyHeader=false, addOffset=true, numPr
     return SET(header, data, path, file)
 end
 
+# Find the EEG dataset in the file
 function find_set_data(fid)
     vars = keys(fid)
     if "EEG" in vars
@@ -73,6 +139,11 @@ function find_set_data(fid)
     end
 end
 
+"""
+    read_set_header(raw::Dict)
+
+Reads the metadata from the raw dictionary provided by MAT.jl and creates a SETHeader object.
+"""
 function read_set_header(raw::Dict)
     rawKeys = keys(raw)
 
@@ -95,11 +166,15 @@ function read_set_header(raw::Dict)
         end
     end
 
+    # Store the original metadata in the header
+    setproperty!(header, :raw, raw)
+
     rawData = raw["data"]
 
     return header, rawData
 end
 
+# Parse channel information into a SETChannels object
 function parse_set_channels(rawchans)
     rawKeys = keys(rawchans)
 
@@ -121,6 +196,7 @@ function parse_set_channels(rawchans)
     return channels
 end
 
+# Convert channel data into vectors
 function normalize_chans(rawVector, outputType)
     return vec(map(x -> isempty(x) ? get_zero(outputType) : x, rawVector))
 end
@@ -129,6 +205,7 @@ get_zero(T::Any) = nothing
 get_zero(T::Type{<:Number}) = zero(T)
 get_zero(T::Type{<:AbstractString}) = ""
 
+# Read data that is stored in the .set file
 function read_set_data(rawData::Array, header::SETHeader, numPrecision, chanSelect, chanIgnore, timeSelect, tasks)
     dims = size(rawData)
     flip = header.nbchan == dims[1] ? true : false
@@ -184,6 +261,7 @@ function read_set_data(rawData::String, args...)
     end
 end
 
+# Read data that is stored in a separate .fdt file
 function read_set_data(rawData::IO, header::SETHeader, numPrecision, chanSelect, chanIgnore, timeSelect, method, tasks)
     rawChans = header.nbchan
     rawSamples = header.pnts
